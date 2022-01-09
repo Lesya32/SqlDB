a961447d3a510d6c89eedd27ac0ba2f8e1592b5d
Lesya32 <Lesya3280@yandex.ru>
2022-01-09 14:17:43 +0800

    a

--- base/SqlDB/views.py
+++ base/SqlDB/views.py
@@ -6,4 +6,4 @@ from .models import Articles
 def views(request):
     news = Articles.objects.all()
     print('views')
-    return render(request, 'SQLDb/index.html', {'news': news})
+    return render(request, 'SqlDB/index.html', {'news': news})
